058ea60bc667ae4e0933bfbe500915cf193542fc
codinsonn <thorr@codinsonn.dev>
2022-05-29 17:12:17 +0200

    feat: Add Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const { withUnimodules } = require('@expo/webpack-config/addons')
+
+module.exports = {
+    stories: [
+        '../apps/**/*.stories.mdx', 
+        '../apps/**/*.stories.@(js|jsx|ts|tsx)',
+        '../packages/**/*.stories.mdx', 
+        '../packages/**/*.stories.@(js|jsx|ts|tsx)',
+    ],
+    addons: ['@storybook/addon-essentials'],
+    webpackFinal: (config) => {
+        // Add TS & react-native-web support
+        config.module.rules.push({
+          test: /\.tsx?$/,
+          use: [
+            {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-typescript'],
+                },
+            },
+          ],
+        })
+        // Aliases
+        config.resolve.alias['react-native$'] = require.resolve('react-native-web')
+        config.resolve.extensions.push('.ts', '.tsx')
+        // Export updated webpack config
+        return withUnimodules(config, {
+          projectRoot: path.resolve(__dirname, '../'),
+        })
+    },
+    core: {
+        builder: 'webpack5',
+    },
+    framework: '@storybook/react',
+}
