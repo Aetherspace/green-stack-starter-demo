784773eaca4f0e1d40bab4d2b6ed07bd4515f6a4
codinsonn <thorr@codinsonn.dev>
2022-11-19 15:02:15 +0100

    feat: Move apps/expo to Expo Router

---
+++ apps/expo/app/index.tsx
@@ -0,0 +1,16 @@
+// Primitives
+import { Text, View } from 'aetherspace/primitives'
+// Screens
+// import { HomeScreen } from 'app/screens/HomeScreen'
+
+/* --- /home ----------------------------------------------------------------------------------- */
+
+const HomeScreen = () => (
+  <View tw="flex-1 bg-white items-center justify-center">
+    <Text tw="text-2xl font-bold">Hello Router 👋</Text>
+  </View>
+)
+
+/* --- Exports --------------------------------------------------------------------------------- */
+
+export default HomeScreen
