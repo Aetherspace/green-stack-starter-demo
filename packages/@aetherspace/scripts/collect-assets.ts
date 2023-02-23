520286be809afd738ac2692550727246bf98d36b
codinsonn <thorr@codinsonn.dev>
2023-02-23 20:55:51 +0100

    fix: Ass collection script

--- packages/@aetherspace/scripts/collect-assets.ts
+++ packages/@aetherspace/scripts/collect-assets.ts
@@ -11,13 +11,14 @@ const collectAssets = () => {
     const excludeJS = (pth) => ['.js', '.json'].every((ext) => !pth.includes(ext))
     const excludeDirs = (pth) => pth.split('/').pop().includes('.')
     // Get all asset file paths in the next app's public folder
-    const assetPaths = glob
-      .sync('../../apps/next/public/**/*')
-      .filter(excludeJS)
-      .filter(excludeDirs)
+    const assetPaths = glob.sync('../../apps/next/public/**/*').filter(excludeJS).filter(excludeDirs) // prettier-ignore
+    // Create symlink
+    fs.rmSync('../../apps/expo/assets/generated', { recursive: true, force: true })
+    fs.mkdirSync('../../apps/expo/assets/generated', { recursive: true })
     // Map asset paths to asset keys and turn into barrel file body for 'assets.generated.ts'
     const assetRegistry = assetPaths.reduce((acc, assetPath) => {
-      const requirePath = assetPath.replace('../../apps/', '../../apps/')
+      const requirePath = assetPath.replace('/next/public', '/expo/assets/generated')
+      fs.cpSync(assetPath, requirePath, { recursive: true })
       const relSrcPath = assetPath.replace('apps/next/public', '')
       const assetKey = getAssetKey(relSrcPath)
       const exportLine = `export const ${assetKey} = require('${requirePath}');`
--- packages/@registries/assets.generated.ts
+++ packages/@registries/assets.generated.ts
@@ -1,6 +1,6 @@
 // -i- Auto generated with "yarn collect-assets"
-export const AdaptiveIconPng = require('../../apps/next/public/adaptive-icon.png');
-export const FaviconPng = require('../../apps/next/public/favicon.png');
-export const IconPng = require('../../apps/next/public/icon.png');
-export const ImgIconPng = require('../../apps/next/public/img/icon.png');
-export const SplashPng = require('../../apps/next/public/splash.png');
+export const AdaptiveIconPng = require('../../apps/expo/assets/generated/adaptive-icon.png');
+export const FaviconPng = require('../../apps/expo/assets/generated/favicon.png');
+export const IconPng = require('../../apps/expo/assets/generated/icon.png');
+export const ImgIconPng = require('../../apps/expo/assets/generated/img/icon.png');
+export const SplashPng = require('../../apps/expo/assets/generated/splash.png');
