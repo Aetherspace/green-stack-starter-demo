f09ec757c1d2de8ada596e51cfa1029268d55a07
codinsonn <thorr@codinsonn.dev>
2023-08-27 22:05:33 +0200

    fix: Wrap AetherPage on mobile with KeyboardAvoidingView

--- packages/@aetherspace/navigation/AetherPage/AetherPage.tsx
+++ packages/@aetherspace/navigation/AetherPage/AetherPage.tsx
@@ -1,4 +1,5 @@
-import { ScrollView } from 'react-native'
+import { useRef, useEffect } from 'react'
+import { ScrollView, KeyboardAvoidingView, Platform, Keyboard } from 'react-native'
 // Types
 import { AetherPageProps, AetherScreenConfig } from './AetherPage.types'
 
@@ -8,17 +9,46 @@ export const AetherPage = <SC extends AetherScreenConfig>(props: AetherPageProps
   // Props
   const { params, screen, screenConfig, ...restProps } = props
 
+  // Refs
+  const scrollViewRef = useRef<ScrollView>(null)
+  const scrollYRef = useRef(0)
+
   // Screen
   const PageScreen = screen
+
+  // -- Keyboard Management --
+
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', () => {
+      setTimeout(() => {
+        scrollViewRef.current?.scrollTo({ x: 0, y: scrollYRef.current + 8, animated: true })
+      }, 300)
+    })
+
+    return () => {
+      keyboardDidShowListener.remove()
+    }
+  }, [])
+
+  // -- Handlers --
+
+  const handleScroll = (e: any) => (scrollYRef.current = e.nativeEvent.contentOffset.y)
 
   // -- Browser --
 
   return (
-    <ScrollView
-      style={{ backgroundColor: screenConfig.backgroundColor || 'transparent' }}
-      contentContainerStyle={{ width: '100%', minHeight: '100%' }}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1, backgroundColor: screenConfig.backgroundColor || 'transparent' }}
     >
-      <PageScreen params={params} {...restProps} />
-    </ScrollView>
+      <ScrollView
+        ref={scrollViewRef}
+        contentContainerStyle={{ width: '100%', minHeight: '100%' }}
+        scrollEventThrottle={16}
+        onScroll={handleScroll}
+      >
+        <PageScreen params={params} {...restProps} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
